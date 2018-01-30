83767c35fed1a7d825844007adb19200f499b938
Matt Shadish <mshadish@sbcglobal.net>
2018-01-29 22:24:22 -0800

    Add auto-commit/push date update script

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ Simple D3 visualization to show how the Treasury Yield curve has changed over ti
 
 ## Usage
 
-To use, hover the mouse over the year display (which provides data for 1990 - today).  With the exception of this year, each year is represented as the average of data points for the year.  This year is represented as the most recent data point.
+To use, hover the mouse over the year display (which provides data for 1990 - today).  Including this year, each year is represented as the average of data points for the year.  The final data point is a representation of the yield curve today.
+
+## Automatic updates
+
+Included sript to refresh the historical treasury data set nightly if there are new data points.  Instead of trying to make a request to the treasury.gov site every time this website is hit, we will instead make a new commit and push whenever we see additional data added to the historical data set.  This is driven by the shell script historicalDataUpdate.sh.
 
 ## To do
 
-Pull data from treasury.gov for up-to-date yield curve data
+Set update to run on cron.  Clean up directory.
---
+++ historicalDataUpdate.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+###############################################################################
+#
+# Bash script to manage commits and updates of historical treasury data
+#
+# Specifically, if it looks like we have more data in the historical data file
+# then we will commit and push to the repo
+#
+###############################################################################
+
+
+# in order to do a proper comparison,
+# first store the old line count of the historical treasury data file
+old_line_count=`wc -l treasury_data_norm.csv | tr -s ' ' | cut -d ' ' -f 2`;
+
+
+
+# next, run the python script to "refresh" the treasury data files
+# ("refresh" because there may not be new data)
+python refreshTreasuryData.py
+
+
+# after running the update,
+# compare the line counts of the old historical data file
+# and the new data file to see if we have more data
+new_line_count=`wc -l treasury_data_norm.csv | tr -s ' ' | cut -d ' ' -f 2`;
+
+
+
+# now compare the old historical data count to the new count
+if [ "$((old_line_count))" -lt "$((new_line_count))" ]
+	# if we now have more records, then commit and push
+	then
+		# add the data files to commit
+		git add treasury_data_norm.csv;
+		git add treasury_data_historical.csv;
+		# commit 
+		git commit --dry-run -m 'Data update `date`';
+		# ...and push
+		git push;
+fi;
